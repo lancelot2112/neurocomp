7b5d504c7c00144aa17bc7a41ae1c2ad2f08b7d1
Lance Faivor <lfaivor21@gmail.com>
2023-12-26 00:09:26 -0500

    Added an entry point

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <errno.h>
+#include <string.h>
+#include <getopt.h>
+#include <sys/types.h>
+
+#include "binvec.h"
+
+int main(int argc, char *argv[]) {
+
+    binvec_t a = binvec_rand(10000, 100);
+    binvec_t b = binvec_rand(10000, 100);
+    binvec_add(a, b);
+    binvec_print(a);
+
+    binvec_free(&a);
+    binvec_free(&b);
+}
